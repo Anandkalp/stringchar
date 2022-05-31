2d3e6cbe98749abc862ebf80708d8ad244f2209b
Anandkalp <anandk.neo@gmail.com>
2022-05-31 21:07:26 +0530

    modified

---
+++ Stringchr.java
@@ -0,0 +1,18 @@
+class CharString
+{
+public static void main(String args[])
+	{
+		String s="Java learning";
+		int count=0;
+
+System.out.println("Length of String : "+s.length());
+		for(int i=0; i<s.length(); i++)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				count++;
+			}
+		}
+		System.out.println("Count of characters : "+count);
+	}
+}
